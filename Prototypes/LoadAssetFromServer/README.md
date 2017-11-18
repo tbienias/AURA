9890dec36fbeb559acd6d35e6f78085ee4aaf54b
JACKSONMEISTER <tim.bienias@gmail.com>
2017-11-18 18:46:13 +0100

    Migrated LoadAssetFromServer prototype to new repository.

---
+++ Prototypes/LoadAssetFromServer/README.md
@@ -0,0 +1,3 @@
+# Prototype: *LoadAssetFromServer*
+## Brief description
+This prototype loads an asset from a HTTPS server and displays it.
---
+++ Prototypes/LoadAssetFromServer/TemplateProject/Assets/Scripts/AssetManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AssetManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        string url = "https://code.dedyn.io/AssetBundles/crate";
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForReq(www));
+    }
+
+    IEnumerator WaitForReq(WWW www)
+    {
+        yield return www;
+        AssetBundle bundle = www.assetBundle;
+        if (www.error == null)
+        {
+            GameObject barrel = (GameObject)bundle.LoadAsset("Crate_Sealed_01");
+            Vector3 pos = new Vector3(0, 0, 2);
+            Quaternion rot = new Quaternion(45.0f, 45.0f, 45.0f, 0.0f);
+            Instantiate(barrel, pos, rot);
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+}
