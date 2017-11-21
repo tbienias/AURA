3a738a6910642370a7d83ead65fad767cdf026ee
faehker.lohl <faehker.lohl@gmail.com>
2017-11-22 00:25:39 +0100

    A little more sophistocated implementation of the LoadAsset-Server

---
+++ Prototypes/BundleServer/README.md
@@ -0,0 +1,3 @@
+# Prototype: *BundleServer*
+## Brief description
+This prototype loads a bundle from a server, checks which files it contains and finally displays it.
---
+++ Prototypes/BundleServer/TemplateProject/Assets/Scripts/CreateAssetBundles.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+
+public class CreateAssetBundles : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
+
+#endif
