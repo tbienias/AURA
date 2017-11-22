ee41355b7525d8a31671fc482b33c39f1040c443
faehker.lohl <faehker.lohl@gmail.com>
2017-11-23 00:33:30 +0100

    Added Deserialization of JSON-Input and showed how the requests are sent to the server

--- Prototypes/BundleServer/Assets/Scripts/AssetManager.cs
+++ Prototypes/BundleServer/Assets/Scripts/AssetManager.cs
@@ -1,23 +1,68 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+[System.Serializable]
+public class AssetBundleListing
+{
+    public AssetBundleItem[] AssetBundleList;    
+}
+
+[System.Serializable]
+public class AssetBundleItem
+{
+    public string BundleName;
+    public string[] Assets;
+}
+
 public class AssetManager : MonoBehaviour
 {
+    private AssetBundleListing mAvailableBundles;
 
     void Start()
     {
-        string url = "https://code.dedyn.io/AssetBundles/crate";
-        WWW www = new WWW(url);
-        StartCoroutine(WaitForReq(www));
+        AssetBundleItem[] items = new AssetBundleItem[2];
+        items[0] = new AssetBundleItem() { BundleName = "Test1", Assets = new string[2] };
+        items[1] = new AssetBundleItem() { BundleName = "Test2" };
+
+        string yay = JsonUtility.ToJson(items[0]);
+
+        // Get all possible AssetBundles from the Server
+        WWW www = new WWW("127.0.0.1");
+        this.StartCoroutine(this.WaitForAssetList(www));
+
+        www = new WWW("127.0.0.1/barrel");
+        this.StartCoroutine(this.WaitForReq(www));
+    }
+
+    IEnumerator WaitForAssetList(WWW src)
+    {
+        yield return src;
+
+        this.mAvailableBundles = JsonUtility.FromJson<AssetBundleListing>(src.text);
+
+        foreach ( AssetBundleItem item in this.mAvailableBundles.AssetBundleList )
+        {
+            string bundleInfo = item.BundleName + " contains Assets:\n";
+            foreach (string assetName in item.Assets)
+            {
+                bundleInfo += "\t" + assetName + "\n";
+            }
+            Debug.Log(bundleInfo);
+        }
     }
 
     IEnumerator WaitForReq(WWW www)
     {
         yield return www;
         AssetBundle bundle = www.assetBundle;
+
+        foreach (string assetName in bundle.GetAllAssetNames())
+            Debug.Log(assetName);
+
         if (www.error == null)
         {
-            GameObject barrel = (GameObject)bundle.LoadAsset("Crate_Sealed_01");
+            GameObject barrel = (GameObject)bundle.LoadAsset("Barrel_Sealed_01");
+
             Vector3 pos = new Vector3(0, 0, 2);
             Quaternion rot = new Quaternion(45.0f, 45.0f, 45.0f, 0.0f);
             Instantiate(barrel, pos, rot);
@@ -27,4 +72,7 @@ public class AssetManager : MonoBehaviour
             Debug.Log(www.error);
         }
     }
+
+
+
 }
